1d1c43746f5f4b1a5dfb8ef8d648839b179d6af5
ariptom <95114854+ariptom@users.noreply.github.com>
2023-03-06 16:31:45 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,24 @@
+function preload () {}
+function setup () {
+    canvas=createCanvas(400,300);
+    canvas.center();
+    video=createCapture(VIDEO);
+    video.hide();
+    posenet=ml5.poseNet(video,modelReady);
+    posenet.on("pose",gotResults);
+}
+function modelReady(){
+    console.log("INITIALIZED!!");
+}
+function gotResults(results){
+    if (results.length>0){
+        console.log(results);
+        
+    }
+}
+function draw(){
+    image(video,0,0,400,300);
+}
+function take_snapshot (){
+    save('myimage.png');
+}
